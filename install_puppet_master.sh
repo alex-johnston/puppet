bd16a0316bac7b34252c579d4c336a2c9779043a
root <root@ip-172-31-8-163.us-east-2.compute.internal>
2017-04-09 05:52:53 +0000

    moving to centos

--- install_puppet_master.sh
+++ install_puppet_master.sh
@@ -1,2 +1,7 @@
 yum install git
 wget "https://pm.puppetlabs.com/cgi-bin/download.cgi?dist=el&rel=7&arch=x86_64&ver=latest"
+mv download.cgi\?dist\=el\&rel\=7\&arch\=x86_64\&ver\=latest  pe.tar.gz
+gunzip  pe.tar.gz
+tar -xf pe.tar
+cd puppet-enterprise-2017.1.1-el-7-x86_64
+./puppet-enterprise-installer
